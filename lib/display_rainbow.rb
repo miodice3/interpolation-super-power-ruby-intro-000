e567dbe654c4c9868dddba30eee3dd3eeb780d1e
Michael I <miodice3@gmail.com>
2020-08-13 19:15:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -11,6 +11,10 @@
 
 bob     = ["Bob", 46, "Juggler", "555-555-5555"]
 
+def print_business_card(person)
+  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "Contact: #{person[3]}"
+end
 
 
 
